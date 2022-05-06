c8609cddbc80d66fe5e1c2df229d85bb72e4f5b9
fahadtariq <abdullahtariqfahad@gmail.com>
2022-05-06 11:16:02 +0500

    uri issue fixed

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: `${process.env.VERCEL_URL}/api/graphql`,
+  uri: process.env.NEXT_PUBLIC_URI,
 })
 
 export default client
